d55a31829835aeb82c7fd28397f3de5f41734a71
1H83R <159144430+1H83R@users.noreply.github.com>
2024-04-27 12:44:37 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,31 +1,34 @@
-﻿int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+﻿using System.ComponentModel.Design;
 
-switch (weekDay)
+int weekDay = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if (weekDay == 0)
 {
-    case 0:
-        Console.WriteLine("Sunday");
-        break;
-    case 1:
-        Console.WriteLine("Monday");
-        break;
-    case 2:
-        Console.WriteLine("Tuseday");
-        break;
-    case 3:
-        Console.WriteLine("Wednesday");
-        break;
-    case 4:
-        Console.WriteLine("Thursday");
-        break;
-    case 5:
-        Console.WriteLine("Friday");
-        break;
-    case 6:
-        Console.WriteLine("Saturday");
-        break;
-    default:
-        Console.WriteLine("Your calendar is broken :(");
-        break;
+    Console.WriteLine("Sunday");
+}
+else if (weekDay == 1)
+{
+    Console.WriteLine("Monday");
+}
+else if (weekDay == 2)
+{
+    Console.WriteLine("Tuestday");
+}
+else if (weekDay == 3)
+{
+    Console.WriteLine("Wednesday");
+}
+else if (weekDay == 4)
+{
+    Console.WriteLine("Thursday");
+}
+else if (weekDay == 5)
+{
+    Console.WriteLine("Friday");
+}
+else
+{
+    Console.WriteLine("Saturday");
 }
 
-Console.WriteLine("Have a nice day");
+Console.WriteLine("Have a nice day!");
